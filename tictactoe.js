054c14f5d43909c12cec386deec160b43c5532f2
Md.Sayem <sayem243@gmail.com>
2017-11-30 19:40:54 +0600

    Assignment-6

--- tictactoe.js
+++ tictactoe.js
@@ -1,109 +1,101 @@
 //JS code goes here
+
+var turn = true;
+
+//declared all false to avoid undefined value conflicts
 var clicked = [
 false, false, false,
 false, false, false,
 false, false, false
 ];
-var markLevel = [
-  0,0,0,0,0,0,0,0,0
-]
 
+//the win array
 var endGame = [
 	[0,1,2],
 	[3,4,5],
 	[6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
+	[0,4,8],
 	[2,4,6],
-	[0,4,8]
+	[1,4,7],
+	[0,3,6],
+	[2,5,8]
 ];
 
-
-var turn = true;
-
 var gameOver = false;
-
-var player1w=0;
-var player2w=0;
 
 function play(event){
 	var element = event.target;
 
 	if ( clicked[element.id] === false && gameOver === false ) {
 
-    element.innerHTML = turn ? "X" : "O";
-    markLevel[element.id] = turn? 1 : 2;
-    clicked[element.id] = true;
-		turn = !turn;
-		//console.log(event);
-
+		clicked[element.id] = turn ? 1 : 0;
 		console.log(clicked);
-    console.log(markLevel);
+		console.log(event);
+
+		element.innerHTML = turn ? "X" : "O";
+		turn = !turn;
 
 		var result = checkEndGame();
 
 		if (result === 1) {
-      player1w++;
 			console.log("Player 1 wins!");
-      document.getElementById('pi').innerHTML = player1w;
-      document.getElementById('pOne').innerHTML = player1w;
-			document.getElementById('gamestatus').innerHTML = "Player 1 win";
+			document.getElementById('status-d').innerHTML = "Player 1 wins!";
 			gameOver = true;
-
-		}
-		if (result === 2) {
-      player2w++;
-			console.log("Player 2 wins!");
-      document.getElementById('pii').innerHTML = player2w;
-      document.getElementById('ptwo').innerHTML = player2w;
-			document.getElementById('gamestatus').innerHTML = "Player 2 wins";
-			gameOver = true;
-
 		}
 		if (result === 0) {
-			console.log("Tie");
-			document.getElementById('gamestatus').innerHTML = "Game Tie!";
+			console.log("Player 2 wins!");
+			document.getElementById('status-d').innerHTML = "Player 2 wins!";
 			gameOver = true;
-
+		}
+		if (result === -1) {
+			console.log("Tie");
+			document.getElementById('status-d').innerHTML = "It's a Tie!";
+			gameOver = true;
 		}
 		if (result === false) {
-			document.getElementById('gamestatus').innerHTML = "Game in Progress";
-			console.log("Continue game");
+			document.getElementById('status-d').innerHTML = "Game in Progress";
+			console.log("Game must go on!");
 		}
 	}
 }
 
+// returns false if game didnt end
+// returns 1 if player 1 is winner; 0 if player 2 is winner
+// returns -1 for draw
+
 function checkEndGame() {
 	for (var i = 0; i < endGame.length; i++) {
 		if (
-			clicked[endGame[i][0]] !== false &&	clicked[endGame[i][1]] !== false &&	clicked[endGame[i][2]] !== false
+			clicked[endGame[i][0]] !== false &&
+			clicked[endGame[i][1]] !== false &&
+			clicked[endGame[i][2]] !== false
 		) {
-			if ( markLevel[endGame[i][0]] === markLevel[endGame[i][1]] && markLevel[endGame[i][1]] === markLevel[endGame[i][2]] ) {
-				if ( markLevel[endGame[i][0]] === 1) {
-
+			if ( clicked[endGame[i][0]] === clicked[endGame[i][1]] && clicked[endGame[i][1]] === clicked[endGame[i][2]] ) {
+				if ( clicked[endGame[i][0]] === 1) {
+					//player 1 winner
 					return 1;
 				}
-				if ( markLevel[endGame[i][0]] === 2) {
-
-					return 2;
+				if ( clicked[endGame[i][0]] === 0) {
+					//player 2 winner
+					return 0;
 				}
 			}
 		}
 	}
 
-
-  var tie = true;
+	//check tie
+	var tie = true;
 	for (var i = 0; i < clicked.length; i++) {
 		if (clicked[i] === false) {
-      tie = false;
-      break;
+			tie = false;
+			break;
 		}
 	}
 
 	if (tie === true) {
 		return -1;
 	}
+
 	return false;
 }
 
@@ -112,28 +104,9 @@ function restart (event){
 		if( clicked[i] !== false ) {
 			document.getElementById(i).innerHTML = '';
 			clicked[i] = false;
-      markLevel[i] = 0;
 		}
 	}
 	turn = true;
 	gameOver = false;
-  player1w = 0;
-  player2w = 0;
-
-  document.getElementById('pi').innerHTML = player1w;
-  document.getElementById('pOne').innerHTML = player1w;
-  document.getElementById('pii').innerHTML = player2w;
-  document.getElementById('ptwo').innerHTML = player2w;
-	document.getElementById("gamestatus").innerHTML = "New Game";
-}
-
-function newGame (event){
-	for(var i = 0; i < clicked.length; i++){
-		document.getElementById(i).innerHTML = '';
-		clicked[i] = false;
-    markLevel[i] = 0;
-	}
-	turn = true;
-	gameOver = false;
-	document.getElementById("gamestatus").innerHTML = "New Game";
+	document.getElementById("status-d").innerHTML = "New Game";
 }
